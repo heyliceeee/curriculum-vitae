cd7102f0bb82685456c5f78e4cb7fc8c27bf327a
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-11-18 14:17:48 +0000

    :lipstick: feat: Estilização CSS conteúdo

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="container-header">
+        <p>Header</p>
+        <p>Second Header</p>
+      </div>
+
+      <br></br>
+
+      <p>About Me</p>
+      <p>Experience</p>
+      <p>Projects</p>
+
+      <br></br>
+      <p>Footer</p>
+    </div>
+  );
+}
+
+export default App;
