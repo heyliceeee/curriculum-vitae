c5387027a9c55ac717da486cc3096681529226e8
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-12-01 00:07:12 +0000

    :lipstick: feat: Estilização CSS experience

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -33,13 +33,7 @@ function App() {
       </div>
 
       <div id='experience' className="container-experience">
-        <Experience
-          itemExperience="Experience" 
-          itemExperienceCompany1="Assistência Online" itemExperienceJob1="Internship" itemExperienceDateStart1="03/2022" itemExperienceDateEnd1="07/2022" itemExperienceDescription1="A platform was created to enable SMS scheduling and sending the SMS automatically."
-          itemExperienceCompany2="ANPRI" itemExperienceJob2="Internship" itemExperienceDateStart2="19/04/2020" itemExperienceDateEnd2="21/05/2020" itemExperienceDescription2="Mobile Development with Web Technology. Proficiency in JavaScript and CSS through the Adobe Brackets platform. Creation of hybrid applications using Adobe PhoneGap."
-          itemExperienceCompany3="Famalicão Extreme Gaming" itemExperienceJob3="Assistant" itemExperienceDateStart3="05/12/2019" itemExperienceDateEnd3="06/12/2019" itemExperienceDescription3="I was responsible for organizing and setting up the ANPRI stand, ensuring the correct arrangement of all equipment and promotional materials, as well as the logistics and coordination of the support team. I led live demonstrations of robots and drones, clarifying their functioning, usefulness and relevance in today's society.I established direct contact with visitors of all ages, from children to adults, educating them about technological advances and fostering interest in the areas of robotics and drones. I actively collaborated with other team members to ensure an educational and engaging experience for all visitors to the stand."
-          itemExperienceCompany4="Apps for Good" itemExperienceJob4="Participant" itemExperienceDateStart4="03/2019" itemExperienceDateEnd4="06/2019" itemExperienceDescription4="I developed and contributed to the creation of the Study+ software, an innovative tool designed to support studying for students. I was responsible for designing the user interface, ensuring an intuitive and pleasant user experience. I collaborated in a multidisciplinary team, improving communication, problem-solving and teamwork skills."
-          itemExperienceCompany5="Lantal Textiles" itemExperienceJob5="Internship" itemExperienceDateStart5="08/2018" itemExperienceDateEnd5="10/2018" itemExperienceDescription5="A search of the company's articles was organized, to later create a Database of articles. The Database was intended to be used for internal use in the company, mainly by the Samples Department." />
+        <Experience itemExperience="Experience" />
       </div>
 
       <div id='projects' className="container-projects">
--- portfolio/src/components/experience/experience.jsx
+++ portfolio/src/components/experience/experience.jsx
@@ -2,6 +2,65 @@ import React, { useState } from 'react';
 import './experience.css';
 
 function Experience(props) {
+    const [activeIndex, setActiveIndex] = useState(1);
+
+    const experiences = [
+        {
+            id: 1,
+            company: "Núcleo Engenharia Informática - ESTG",
+            job: "Web Development Team Member",
+            dateStart: "Nov 2023",
+            dateEnd: "Now",
+            description: "Development of the 'Núcleo Engenharia de Informática - ESTG' website.",
+        },
+        {
+            id: 2,
+            company: "Assistência Online",
+            job: "Internship",
+            dateStart: "Mar 2022",
+            dateEnd: "Jul 2022",
+            description: "A platform was created to enable SMS scheduling and sending the SMS automatically.",
+        },
+        {
+            id: 3,
+            company: "ANPRI",
+            job: "Internship",
+            dateStart: "Apr 2020",
+            dateEnd: "May 2020",
+            description: "Mobile Development with Web Technology. Proficiency in JavaScript and CSS through the Adobe Brackets platform. Creation of hybrid applications using Adobe PhoneGap."
+
+        },
+
+        {
+            id: 4,
+            company: "Famalicão Extreme Gaming",
+            job: "Assistant",
+            dateStart: "5 Dec 2019",
+            dateEnd: "6 Dec 2019",
+            description: "I organized and set up the ANPRI booth, ensuring equipment and materials were arranged correctly. Led live demonstrations of robots and drones, explaining their functionality and relevance. Interacted with visitors of all ages, educating them on technological advances in robotics and drones. Collaborated with the team to create an engaging experience for all booth visitors."
+        },
+        {
+            id: 5,
+            company: "Apps for Good",
+            job: "Participant",
+            dateStart: "Mar 2019",
+            dateEnd: "Jun 2019",
+            description: "I developed and contributed to the creation of the Study+ software, an innovative tool designed to support studying for students. I was responsible for designing the user interface, ensuring an intuitive and pleasant user experience. I collaborated in a multidisciplinary team, improving communication, problem-solving, and teamwork skills."
+        },
+        {
+            id: 6,
+            company: "Lantal Textiles",
+            job: "Internship",
+            dateStart: "Set 2018",
+            dateEnd: "Oct 2018",
+            description: "A search of the company's articles was organized, to later create a Database of articles. The Database was intended to be used for internal use in the company, mainly by the Samples Department."
+        }
+    ];
+
+    const handleExperienceClick = (experienceId) => {
+        setActiveIndex(experienceId == activeIndex ? 1 : experienceId);
+    };
+
 
     return (
         <div className='containerExperience'>
@@ -9,27 +68,40 @@ function Experience(props) {
 
             <div className='containerInfo'>
                 <div className='containerCompaniesExperience'>
-                    <div className='containerCompanyExperience'>
-                        <div className='line'></div>
+                    {experiences.map((experience) => (
 
-                        <div className='containerCompanyName'>
-                            <div className='titleCompanyName'>{props.itemExperienceCompany1}</div>
+                        <div key={experience.id} className='containerCompanyExperience' onClick={() => handleExperienceClick(experience.id)}>
+                            <div className={experience.id === activeIndex ? 'line' : 'lineDisable'}></div>
+
+                            <div className={experience.id === activeIndex ? 'containerCompanyName' : 'containerCompanyNameDisable'}>
+                                <div className={experience.id === activeIndex ? 'titleCompanyName' : 'titleCompanyNameDisable'}>{experience.company}</div>
+                            </div>
                         </div>
-                    </div>
+
+                    ))}
                 </div>
 
                 <div className='containerDescriptionsExperience'>
-                    <div className='containerDescriptionExperience'>
-                        <div className='containerJobDate'>
-                            <div className='titleJob'>{props.itemExperienceJob1}</div>
+                    {experiences.map((experience) => (
+                        activeIndex === experience.id && (
+                            <div key={experience.id} className='containerDescriptionExperience'>
 
-                            <div className='containerDate'>
-                                <div className='dateStart'>{props.itemExperienceDateStart1}</div>
-                                <div className='dateStart'> - </div>
-                                <div className='dateEnd'>{props.itemExperienceDateEnd1}</div>
+                                <div className='containerJobDate'>
+                                    <div className='titleJob'>{experience.job}</div>
+                                    
+                                    <div className='containerDate'>
+                                        <div className='dateStart'>{experience.dateStart}</div>
+                                        <div className='dateStart'> - </div>
+                                        <div className='dateEnd'>{experience.dateEnd}</div>
+                                    </div>
+                                </div>
+
+                                <div className='containerCompany'>{experience.company}</div>
+
+                                <div className='containerDescription'>{experience.description}</div>
                             </div>
-                        </div>
-                    </div>
+                        )))}
+
                 </div>
             </div>
         </div>
