5b884221b186bdc9b72d25f8bb1c7eb745354847
heyliceeee <alicedias2002@hotmail.com>
2025-01-22 15:28:44 +0000

    💄 add projects

--- portfolio/src/components/projects/projects.jsx
+++ portfolio/src/components/projects/projects.jsx
@@ -7,6 +7,58 @@ function Projects(props) {
     const projects = [
         {
             id: 1,
+            photo: "restaurant-dashboard.jpg",
+            name: "Restaurant - Dashboard",
+            description: "Website to manage and view information about restaurants.",
+            tecnologies: [
+                { name: "Vue.js" },
+                { name: "JSON Server" },
+                { name: "Ant Design" },
+                { name: "ECharts" },
+                { name: "JavaScript" },
+            ],
+            URL: "https://github.com/heyliceeee/vuejs-course"
+        },
+        {
+            id: 2,
+            photo: "sd.png",
+            name: "Distributed Communication System",
+            description: "Distributed communication system for emergency coordination scenarios.",
+            tecnologies: [
+                { name: "TCP" },
+                { name: "Java" },
+                { name: "RSA" },
+                { name: "Multithreading" },
+            ],
+            URL: "https://github.com/heyliceeee/SD-trabalhoPratico"
+        },
+        {
+            id: 3,
+            photo: "tocruz.png",
+            name: "Mission Simulator - To Cruz",
+            description: "Map-based mission simulator with several interconnected divisions.",
+            tecnologies: [
+                { name: "ADT" },
+                { name: "Java" },
+                { name: "JavaFX" },
+            ],
+            URL: "https://github.com/heyliceeee/simulador-missoes-to-cruz"
+        },
+        {
+            id: 4,
+            photo: "si-levexpress.png",
+            name: "LevExpress - Delivery Management",
+            description: "System developed for efficient delivery management.",
+            tecnologies: [
+                { name: "Camunda" },
+                { name: "Spring Boot" },
+                { name: "Java" },
+                { name: "Task Automation" },
+            ],
+            URL: "https://github.com/heyliceeee/SI-levExpress"
+        },
+        {
+            id: 5,
             photo: "crossfit-cards.png",
             name: "Crossfit Cards",
             description: "Web application that turns CrossFit workouts into a dynamic card game.",
@@ -19,7 +71,7 @@ function Projects(props) {
             URL: "https://github.com/heyliceeee/crossfit-cards"
         },
         {
-            id: 1,
+            id: 6,
             photo: "sweetRicers.png",
             name: "Sweet Ricers",
             description: "Website about a Portuguese Counter Strike 2 community.",
@@ -32,7 +84,7 @@ function Projects(props) {
             URL: "https://sweet-ricers.vercel.app/"
         },
         {
-            id: 2,
+            id: 7,
             photo: "robocode.jpg",
             name: "Robocode",
             description: "Robot controlled by Machine Learning and Genetic Algorithm.",
@@ -45,7 +97,7 @@ function Projects(props) {
             URL: "https://github.com/heyliceeee/robocodeIA"
         },
         {
-            id: 3,
+            id: 8,
             photo: "nei.png",
             name: "NEI Website",
             description: "Development of the 'Núcleo de Estudantes de Informática - Escola de Tecnologia e Gestão de Felgueiras' website.",
@@ -57,7 +109,7 @@ function Projects(props) {
             URL: "https://nei-estg.org/"
         },
         {
-            id: 4,
+            id: 9,
             photo: "trazCa.png",
             name: "'Traz Cá' - Food Delivery App",
             description: "A platform for iOS and Android that facilitates the food ordering process, promoting a fluid experience between restaurants and customers, encouraging local commerce and providing a convenient meal solution for users.",
@@ -69,7 +121,7 @@ function Projects(props) {
             URL: "https://github.com/heyliceeee/trazcaApp"
         },
         {
-            id: 5,
+            id: 10,
             photo: "captureTheFlag.jpg",
             name: "Capture the Flag Game",
             description: "Game developed with generic ADT.",
@@ -80,7 +132,7 @@ function Projects(props) {
             URL: "https://github.com/heyliceeee/capture-the-flag"
         },
         {
-            id: 6,
+            id: 11,
             photo: "generatePassword.png",
             name: "Password Generator",
             description: "A platform for iOS and Android that provide a safe and effective tool for generating passwords.",
@@ -93,7 +145,7 @@ function Projects(props) {
             URL: "https://github.com/heyliceeee/passwordGenerator"
         },
         {
-            id: 7,
+            id: 12,
             photo: "spaceFlightNews.png",
             name: "Space Flight News",
             description: "This is an iOS application allows users to access news and information about the aerospace field. The app utilizes an API to provide updated data on rocket launches, space missions, astronomical discoveries, and more.",
@@ -105,7 +157,7 @@ function Projects(props) {
             URL: "https://github.com/heyliceeee/spaceFlightNews"
         },
         {
-            id: 8,
+            id: 13,
             photo: "smsSystem.png",
             name: "SMS System",
             description: "The SMS System is a scheduled and automatic SMS sending system. It is a project that constitutes: API, Database, Backend and App. The system was developed during the 'Assistência Online' internship and was inspired by the problem of a company customer.",
@@ -119,6 +171,7 @@ function Projects(props) {
             URL: "#"
         },
     ];
+    
 
     const backgroundImageStyle = (photo) => ({
         backgroundImage: `url(${require('../../assets/' + photo)})`,
